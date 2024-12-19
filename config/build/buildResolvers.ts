a9bad93f1a5484684f039911bb42cedc2c642300
Bayel Ibraev <bayelibraev1@gmail.com>
2024-12-19 08:18:45 +0600

    first stage end

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,15 @@
-import {ResolveOptions} from "webpack";
+import { ResolveOptions } from "webpack";
+import { BuildOptions } from "./types/config";
+import path from "path";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers({ paths }: BuildOptions): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true,
+        modules: [path.resolve(paths.src), 'node_modules'],
+        mainFiles: ['index'],
+        alias: {
+            '@': path.resolve(paths.src)
+        },
     }
 }
