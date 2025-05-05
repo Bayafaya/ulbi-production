571b6cc675e4c08775ca3904041c28131f9723f6
bayel_ibraev <bayelibraev1@gmail.com>
2025-05-05 17:57:54 +0600

    add eslint, fix all files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss';
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar";
-import {Sidebar} from "widgets/Sidebar";
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTheme} from 'app/providers/ThemeProvider';
+import {AppRouter} from 'app/providers/router';
+import {Navbar} from 'widgets/Navbar';
+import {Sidebar} from 'widgets/Sidebar';
 
 
 const App = () => {
@@ -12,9 +12,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
+            <Suspense fallback=''>
                 <Navbar />
-                <div className="content-page">
+                <div className='content-page'>
                     <Sidebar />
                     <AppRouter />
                 </div>
